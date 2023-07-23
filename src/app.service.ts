e0c5254b315ecc63030a4c09e737f5e6f4d08fc8
seryozha_now <baleyko.s@promail366.com>
2023-07-23 21:01:23 +0300

    json db

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Telegraf, Context } from 'telegraf';
-import { message } from 'telegraf/filters';
+import { chromium } from 'playwright';
+import { JsonDB, Config } from 'node-json-db';
+
+interface Article {
+    title: string;
+    date: string;
+}
+
+const db = new JsonDB(new Config('db', true, false, '/'));
 
 @Injectable()
 export class AppService {
@@ -12,8 +20,36 @@ export class AppService {
             this.configService.get('TELEGRAM_BOT_TOKEN'),
         );
 
-        this.bot.on(message('text'), async (ctx) => {
-            await ctx.reply(`Здарова заебал!`);
+        this.bot.command('start', (ctx) => {
+            (async () => {
+                const browser = await chromium.launch({
+                    headless: true,
+                });
+
+                const context = await browser.newContext();
+                const page = await context.newPage();
+                await page.goto('https://web.dev/blog');
+                const articles = await page.$$('article');
+
+                for (const article of articles) {
+                    const item: Article = {
+                        title: await article.$eval(
+                            'h2 > a',
+                            (el) => el.textContent,
+                        ),
+                        date: await article.$eval(
+                            'time',
+                            (el) => el.textContent,
+                        ),
+                    };
+
+                    db.push('/articles[]', item);
+                }
+
+                await browser.close();
+            })();
+
+            return ctx.reply('completed');
         });
 
         this.bot.launch();
